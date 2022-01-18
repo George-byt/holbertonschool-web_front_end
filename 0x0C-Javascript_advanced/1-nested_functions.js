178ce2641b150bc79f08efe51e388cee6f400020
George-byt <jmoralesvalencia750@gmail.com>
2022-01-18 11:22:33 -0500

    Closure Scope Chain

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,15 @@
+let globalVariable = 'Welcome';
+const outer = () => {
+    let course = 'Holberton';
+    alert(globalVariable);
+
+    const inner = () => {
+        let exclamation = '!';
+        alert(`${globalVariable}  ${course}`)
+        
+        const inception = () => alert(`${globalVariable} ${course} ${exclamation}`);
+        inception();
+    }
+    inner();
+}
+outer();
